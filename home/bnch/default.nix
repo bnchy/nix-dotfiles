4bbbb1782e47074325f732e75bffef5d0ed17e30
bnchy <s123836@ap.be>
2024-02-15 16:25:02 +0100

    feat: implemented stylix

--- home/bnch/default.nix
+++ home/bnch/default.nix
@@ -3,15 +3,31 @@
     ./applications/vscode.nix
     ./applications/intellij.nix
     ./applications/git.nix
+    ./applications/discord.nix
+
+    ./cli/direnv.nix
+
+    ./stylix.nix
   ];
 
   nixpkgs.config = {
     allowUnfree = true;
     allowUnfreePredicate = _: true;
   };
+  
+  programs.alacritty.enable = true;
 
   home = {
-    packages = with pkgs; [firefox];
+    packages = with pkgs; [
+      firefox
+      slack
+      spotify
+      nodejs
+      libreoffice-qt
+      hunspell
+      hunspellDicts.nl_NL
+      neofetch
+    ];
     stateVersion = "23.11";
   };
 }
